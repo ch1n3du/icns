7e5ef23a27bb3b25582b22a3d61d78991f44b2c3
ch1n3du <danielonyesoh@gmail.com>
2024-03-18 12:17:06 +0100

    Initial commit.

---
+++ src/icns_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { icns_backend } from "../../declarations/icns_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await icns_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
